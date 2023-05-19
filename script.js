c1bce6a9bdceae796149f11d933a2c87edf9cb62
Levy Cordeiro <108874182+lixoreciclavel@users.noreply.github.com>
2023-05-19 11:03:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var counterVal = 0;
+
+function incrementClick() {
+    document.getElementById("#coracao");
+    updateDisplay(++counterVal);
+}
+
+function resetCounter() {
+    counterVal = 0;
+    updateDisplay(counterVal);
+}
+
+function updateDisplay(val) {
+    document.getElementById("counter-label").innerHTML = val;
+}
+
